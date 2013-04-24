6c8f06630ad261a168ac1517b7aeef1c70c902be
Michael Bestas <mikeioannina@gmail.com>
2013-04-25 00:04:50 +0300

    anzu: rename logo for nAa 3.0.x
    Signed-off-by: Michael Bestas <mikeioannina@gmail.com>
    
    Change-Id: I34d9e23e332f4c17de988597313f6ede1e36b13e

--- anzu.mk
+++ anzu.mk
@@ -14,7 +14,7 @@ PRODUCT_AAPT_PREF_CONFIG := hdpi
 PRODUCT_COPY_FILES += \
     device/semc/anzu/prebuilt/init.semc.usb.rc:root/init.semc.usb.rc \
     device/semc/anzu/prebuilt/hw_config.sh:system/etc/hw_config.sh \
-    device/semc/msm7x30-common/prebuilt/logo_H.rle:root/logo.rle
+    device/semc/msm7x30-common/prebuilt/logo_H.rle:root/initlogo.rle
 
 # Device specific part for two-stage boot
 PRODUCT_COPY_FILES += \
