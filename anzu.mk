746e53ba13d7c8d1f3dc0c182b93f5ab267f2e61
Michael Bestas <mikeioannina@gmail.com>
2013-05-18 15:26:25 +0300

    anzu: Initial reorganization of files
    Move files around like newer sony phones
    
    Signed-off-by: Michael Bestas <mikeioannina@gmail.com>
    
    Change-Id: I19f2be8987df8aa4822d283c699b5ece30869406

--- anzu.mk
+++ anzu.mk
@@ -12,22 +12,19 @@ PRODUCT_AAPT_PREF_CONFIG := hdpi
 
 # Init files
 PRODUCT_COPY_FILES += \
-    device/semc/anzu/prebuilt/init.semc.usb.rc:root/init.semc.usb.rc \
-    device/semc/anzu/prebuilt/hw_config.sh:system/etc/hw_config.sh \
-    device/semc/msm7x30-common/prebuilt/logo_H.rle:root/initlogo.rle
+    $(LOCAL_PATH)/rootdir/init.semc.usb.rc:root/init.semc.usb.rc \
+    $(LOCAL_PATH)/rootdir/system/etc/hw_config.sh:system/etc/hw_config.sh
 
 # Device specific configs
 PRODUCT_COPY_FILES += \
-    device/semc/anzu/config/cy8ctma300_touch.idc:system/usr/idc/cy8ctma300_touch.idc \
-    device/semc/anzu/config/atdaemon.kl:system/usr/keylayout/atdaemon.kl \
-    device/semc/anzu/config/msm_pmic_pwr_key.kl:system/usr/keylayout/msm_pmic_pwr_key.kl \
-    device/semc/anzu/config/pm8058-keypad.kl:system/usr/keylayout/pm8058-keypad.kl \
-    device/semc/anzu/config/simple_remote.kl:system/usr/keylayout/simple_remote.kl \
-    device/semc/anzu/config/init.qcom.bt.sh:system/etc/init.qcom.bt.sh \
-    device/semc/anzu/config/sensors.conf:system/etc/sensors.conf \
-    device/semc/anzu/config/tiwlan.ini:system/etc/tiwlan.ini
-
-$(call inherit-product, device/semc/msm7x30-common/prebuilt/resources-hdpi.mk)
+    $(LOCAL_PATH)/rootdir/system/usr/idc/cy8ctma300_touch.idc:system/usr/idc/cy8ctma300_touch.idc \
+    $(LOCAL_PATH)/rootdir/system/usr/keylayout/atdaemon.kl:system/usr/keylayout/atdaemon.kl \
+    $(LOCAL_PATH)/rootdir/system/usr/keylayout/msm_pmic_pwr_key.kl:system/usr/keylayout/msm_pmic_pwr_key.kl \
+    $(LOCAL_PATH)/rootdir/system/usr/keylayout/pm8058-keypad.kl:system/usr/keylayout/pm8058-keypad.kl \
+    $(LOCAL_PATH)/rootdir/system/usr/keylayout/simple_remote.kl:system/usr/keylayout/simple_remote.kl \
+    $(LOCAL_PATH)/rootdir/system/etc/init.qcom.bt.sh:system/etc/init.qcom.bt.sh \
+    $(LOCAL_PATH)/rootdir/system/etc/sensors.conf:system/etc/sensors.conf \
+    $(LOCAL_PATH)/rootdir/system/etc/tiwlan.ini:system/etc/tiwlan.ini
 
 # Device properties
 PRODUCT_PROPERTY_OVERRIDES += \
