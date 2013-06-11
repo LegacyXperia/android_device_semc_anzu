f0cbe6a503c0a176729754fe77ea9b133726abe0
Michael Bestas <mikeioannina@gmail.com>
2013-06-11 13:45:38 +0300

    Fix build
    Change-Id: Ib690ec5307f7c9b9a9f9ca97bf1259417617af62
    Signed-off-by: Michael Bestas <mikeioannina@gmail.com>

--- full_anzu.mk
+++ full_anzu.mk
@@ -18,6 +18,10 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from anzu device
 $(call inherit-product, device/semc/anzu/anzu.mk)
 
+# Boot Animation
+TARGET_SCREEN_HEIGHT := 854
+TARGET_SCREEN_WIDTH := 480
+
 # Set those variables here to overwrite the inherited values.
 PRODUCT_NAME := full_anzu
 PRODUCT_DEVICE := anzu
