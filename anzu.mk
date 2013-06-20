0c58f588110a0d7913625125e69b9221917c30dd
Michael Bestas <mikeioannina@gmail.com>
2013-06-20 19:40:50 +0300

    Proprietary side needs to exist

--- anzu.mk
+++ anzu.mk
@@ -13,7 +13,7 @@
 # limitations under the License.
 
 # proprietary side of the device
-$(call inherit-product-if-exists, vendor/semc/anzu/anzu-vendor.mk)
+$(call inherit-product, vendor/semc/anzu/anzu-vendor.mk)
 
 DEVICE_PACKAGE_OVERLAYS += device/semc/anzu/overlay
 
