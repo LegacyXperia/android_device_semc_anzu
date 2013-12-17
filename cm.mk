8f7ff6f59da10d7b9b39aeef9de14ae42bab2118
Michael Bestas <mikeioannina@gmail.com>
2013-12-17 02:25:57 +0200

    Add LX build tag

--- cm.mk
+++ cm.mk
@@ -39,3 +39,6 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
 # Device identifier. This must come after all inclusions
 PRODUCT_NAME := cm_anzu
 PRODUCT_DEVICE := anzu
+
+# Custom tag for unofficial builds
+TARGET_UNOFFICIAL_BUILD_ID := LegacyXperia
