084ff9e397f8bd794544136ae647aa0622155951
Michael Bestas <mikeioannina@gmail.com>
2014-02-07 17:04:49 +0200

    anzu: Fix device name
    Change-Id: I11ebb46bda8eff320dfcdef1b800b2f972c78ebc

--- cm.mk
+++ cm.mk
@@ -33,12 +33,15 @@ PRODUCT_PACKAGES += \
 # Set build fingerprint / ID / Product Name ect.
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=LT18i \
+    PRODUCT_DEVICE=LT18i \
     BUILD_FINGERPRINT="SEMC/LT18i_1254-2184/LT18i:4.0.4/4.1.B.0.587/tL1_3w:user/release-keys" \
     PRIVATE_BUILD_DESC="LT18i-user 4.0.4 4.1.B.0.587 tL1_3w test-keys"
 
 # Device identifier. This must come after all inclusions
 PRODUCT_NAME := cm_anzu
-PRODUCT_DEVICE := anzu
+
+# Release name
+PRODUCT_RELEASE_NAME := anzu
 
 # Custom tag for unofficial builds
 TARGET_UNOFFICIAL_BUILD_ID := LegacyXperia
